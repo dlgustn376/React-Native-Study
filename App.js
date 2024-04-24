edf6dd7fccb508cb5e5a83b1f55276d7c7e71c18
HYUNSULEE\dlgustn3768 <dlgustn3768@naver.com>
2024-04-24 16:24:55 +0900

    RN 2-20 study

--- App.js
+++ App.js
@@ -1,25 +1,35 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Button, StyleSheet, Text, View, TextInput } from 'react-native';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text style={styles.textContainer}>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-
+    <View style={styles.appContainer}>
+      <View style={styles.inputContainer}>
+        <TextInput style={styles.textInput} placeholder='Your course goal!' />
+        <Button title='Add Goal' />
+      </View>
+      <View>
+        <Text>List of goals...</Text>
+      </View>
     </View>
   );
 }
 
+
+
+
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+  appContainer: {
+    padding: 50,
   },
-  textContainer: {
-    borderWidth: 2,
-    borderColor: 'red'
-  }
+  inputContainer: {
+    flexDirection: 'row',
+    justifyContent: 'space-between'
+  },
+  textInput: {
+    borderWidth: 1,
+    borderColor: '#cccccc',
+    width:'80%',
+    marginRight: 8,
+    padding: 8,
+  },
 });
