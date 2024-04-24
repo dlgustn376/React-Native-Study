8f19b608efed4fe27543e2a3e9e97d1f92ecb79d
HYUNSULEE\dlgustn3768 <dlgustn3768@naver.com>
2024-04-24 14:53:11 +0900

    react-native expo

---
+++ App.js
@@ -0,0 +1,25 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.textContainer}>Open up App.js to start working on your app!</Text>
+      <StatusBar style="auto" />
+
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  textContainer: {
+    borderWidth: 2,
+    borderColor: 'red'
+  }
+});
